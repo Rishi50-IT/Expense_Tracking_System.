98329050a946cd0131c1c87d63f9c986e1332e69
Rishi Munda <rishimunda50@gmail.com>
2026-05-29 15:27:16 +0530

    Add files via upload

---
+++ pages/➕💸 Add Transations.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import pandas as pd
+import pymongo
+from datetime import date
+import streamlit.components.v1 as com
+from streamlit_lottie import st_lottie
+
+#live server connection
+conn= pymongo.MongoClient("mongodb+srv://Rishi_Munda:Rahul124@@cluster0.dod06ln.mongodb.net/?appName=Cluster0")
+
+
+#for local db
+#conn= pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.3")
+mydb=conn["expensedb"]
+transactions=mydb["transactions"]
+
+st.set_page_config(page_title="Transaction")
+if "transactions" not in st.session_state:
+    st.session_state.transactions = []
+st.header("➕ Add New Transaction")
+c1,c2=st.columns(2)
+with c1:
+    com.iframe("https://lottie.host/embed/135ca7d0-9a8e-41e2-9a8b-3985fd3a4530/0FWKtPLPAv.lottie")
+with c2:
+    com.iframe("https://lottie.host/embed/5aed7e16-d179-449b-80c1-d8ef118f27f0/z7O82KECUH.lottie")
+    
+t1,t2=st.tabs(["💵Add Income","Add Expense"])
+with t1:
+    st.title("Add Income ")
+    
+    
+    with st.form("Add Income"):
+        st.subheader("Add Your  Source of Income")
+        com.iframe("https://lottie.host/embed/bfc96117-3415-4682-8141-5a62ce022119/q2M3kpnaAK.lottie")
+        category= st.selectbox("Select Category" ,["Salary","Business","Invetsment", "Other Source of Income"] )
+        amount = st.number_input("Enter Amount",min_value=0,step=50)
+        month = st.selectbox("Select Month",["January","February","March","April","May","June","July","August","September", "October","November","December"])
+        income_date = st.date_input("Select Date",value=date.today())
+        payment_method= st.radio("Payment Method" ,[ "Cash","Upi","Debit Card","Credit Card"])
+        description = st.text_area("Description / Notes")     
+
+    
+        if st.form_submit_button("Add Income"):          
+           income = {"email":st.session_state["Email"],
+               "Type":"Income","Category": category,"Amount": amount,"Month": month,"Date":str( income_date),"Payment": payment_method,"Description": description}
+
+           transactions.insert_one(income)
+           st.success("Income Added Successfully ✅ ")
+
+        st.markdown("---")
+
+
+
+
+with t2:
+
+    st.set_page_config(
+    page_title="Expense Tracker",
+    page_icon="💰",
+    layout="centered")
+
+    st.title("💰 Add Expense")
+
+    with st.form("expense_form"):
+        com.iframe("https://lottie.host/embed/cf361bf1-f911-43d0-9d81-7aac48e58483/P85yY7w2Kn.lottie")
+        category = st.selectbox( "Expense  Category",["Food","Travel","Shopping",
+                                "Bills","Entertainment","E-commerce",
+                                "Education","Health","Other"])
+
+        amount = st.number_input(
+        "💵 Enter Amount",
+        min_value=0,
+        step=50,
+        key="expense_amount")
+
+        month = st.selectbox("Select Expense Month",["January","February",
+                                                 "March","April","May",
+                                                 "June","July","August",
+                                                 "September", "October",
+                                                 "November","December"],key="expense_month")
+        expense_date = st.date_input("Expense Date",value=date.today(),key="expense_date")
+        payment_method = st.radio("Payment Method",["Cash","UPI","Debit Card","Credit Card"],key="expense_payment")
+        description = st.text_area("Expense Description / Notes",key="expense_descriptionn")  
+
+
+        if st.form_submit_button("Add Expense"):
+            expense={"email":st.session_state["Email"],
+                "Type": "Expense",
+                "Category": category,
+                "Amount": amount,
+                "Month": month,
+                "Date":str( expense_date),
+                "Payment": payment_method,
+                "Description": description}
+            transactions.insert_one(expense)
+            st.success("Expense Added Successfully ✅")
+        else:
+            st.info("No Expense  added yet.")
+        
+
+
+
+
+st.markdown("---")
+
+st.subheader("📋 All Transactions")
+data=list(transactions.find( { "email": st.session_state["Email"] }, {"_id": 0} ))
+
+if data:
+    df = pd.DataFrame(data)
+    st.dataframe(df, use_container_width=True)
+
+
+
+
+else:
+    st.info("No Transactions Added Yet")
+
+st.subheader("📑Income Records")
+if st.button("📊 Data Visulization"):
+    st.switch_page("pages/📋📊Reports.py")
---
+++ pages/📋📊Reports.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pandas as pd
+import pymongo
+import matplotlib.pyplot as plt
+import streamlit.components.v1 as com
+from streamlit_lottie import st_lottie 
+
+
+#live server connection
+conn= pymongo.MongoClient("mongodb+srv://Rishi_Munda:Rahul124@@cluster0.dod06ln.mongodb.net/?appName=Cluster0")
+
+#conn= pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.3")
+
+mydb = conn["expensedb"]
+transactions = mydb["transactions"]
+
+st.title("📊 Reports & Analytics")
+com.iframe("https://lottie.host/embed/a48fea96-bcae-4f62-aee8-e1c07e4f1e59/A5QzK0qPFf.json")
+
+if "Email" not in st.session_state:
+    st.error("Please login to view your reports.")
+    st.stop()
+
+
+records=list(transactions.find({"email":st.session_state["Email"]},{"_id": 0}))
+
+
+
+if not records:
+    st.warning("No Transactions Available")
+
+else:
+    data=pd.DataFrame(records)
+
+    st.subheader("📋 Transaction Table")
+
+    st.dataframe(data, use_container_width=True)
+
+    income = data[data["Type"] == "Income"]["Amount"].sum()
+    expense = data[data["Type"] == "Expense"]["Amount"].sum()
+    balance = income - expense
+
+    c1, c2, c3 = st.columns(3)
+
+    c1.metric("💵 Total Income", f"₹{income}","100%")
+    c2.metric("💸 Total Expense", f"₹{expense}","-9%")
+    c3.metric("💰 Balance", f"₹{balance}","91%")
+
+    st.markdown("---")
+    expense_data = data[data["Type"] == "Expense"]
+if not expense_data.empty:
+        
+        st.subheader("📊 Category Wise Expenses")
+
+        category_data = expense_data.groupby("Category")["Amount"].sum()
+ 
+        fig, ax = plt.subplots()
+
+        ax.bar(category_data.index, category_data.values)
+
+        plt.xticks(rotation=45)
+
+        st.pyplot(fig)
+
+    
+   
+
+st.subheader("🥧 Expense Distribution")
+
+fig2, ax2 = plt.subplots()
+
+ax2.pie(
+        category_data.values,
+        labels=category_data.index,
+        autopct="%1.1f%%"
+    )
+
+st.pyplot(fig2)
+
+
+st.subheader("📈 Monthly Transactions")
+
+monthly = data.groupby("Month")["Amount"].sum()
+
+st.line_chart(monthly)
+
+
+st.subheader("💹 Income vs Expense")
+
+compare = data.groupby("Type")["Amount"].sum()
+
+st.bar_chart(compare)
